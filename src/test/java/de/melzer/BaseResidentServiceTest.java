533f35fa0502c857740107df78559e39db2b7306
Tizian Melzer <tizian.mel@gmail.com>
2018-06-05 14:37:12 +0200

    test

--- src/test/java/de/melzer/BaseResidentServiceTest.java
+++ src/test/java/de/melzer/BaseResidentServiceTest.java
@@ -1,7 +1,27 @@
 package de.melzer;
 
+
+import de.hs_furtwangen.informatik.meldeauskunft.repository.ResidentRepository;
+import de.hs_furtwangen.informatik.meldeauskunft.service.BaseResidentService;
+import org.hamcrest.*;
+
 /**
  * Created by Tizian Melzer on 29.05.2018.
  */
+
 public class BaseResidentServiceTest {
+
+    public void testTransfer(){
+        BaseResidentService residentService = new BaseResidentService();
+        ResidentRepository stub = new ResidentRepositoryStub();
+
+        residentService.setResidentRepository(new ResidentRepositoryStub());
+
+
+         residentService.getFilteredResidentsList(stub.getResidents().get(0));
+    }
+
+
+
+
 }
